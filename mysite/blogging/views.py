4c76a3164a236647d0c070cead6ada278614ecf8
kanmenlaohan <kanmenlaohan@gmail.com>
2019-11-07 23:33:23 -0600

    some changes made

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
+from blogging.models import Post
 
-# Create your views here.
+
+def list_view(request):
+    context = {'posts': Post.objects.all()}
+    return render(request, 'list.html', context)
+
+
+def detail_view(request, blog_id):
+    try:
+        post = Post.objects.get(pk=blog_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    context = {'posts': post}
+    return render(request, 'detail.html', context)
