211683bf5e5fb680440df3fe228436465463dc05
kanmenlaohan <kanmenlaohan@gmail.com>
2019-11-16 17:09:36 -0600

    Lession 7 activity

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import list_view, detail_view
+from blogging.views import list_view, detail_view, stub_view
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
+    path('blogging', stub_view, name="stub_view"),
     path('blog/<int:post_id>/', detail_view, name="blog_detail"),
 ]
--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render
-from django.http import Http404
-from blogging.models import Post
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post, Category
+
+
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
 
 
 def list_view(request):
-    context = {'posts': Post.objects.all()}
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
     return render(request, 'blogging/list.html', context)
 
 
 def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
     try:
-        post = Post.objects.get(pk=post_id)
+        post = published.get(pk=post_id)
+    #    category = published.Category.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
-
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
