ccea0bd0077f132cc95200613671c1fee8008c06
kanmenlaohan <kanmenlaohan@gmail.com>
2019-11-16 22:04:47 -0600

    fix something

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('polling/', include('polling.urls')),
+    path('', include('polling.urls')),
     path('blogging/', include('blogging.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
